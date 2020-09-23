e49253fb3d14c82ea40c607df32f6b118ed2b08f
oudi2012 <wanghaikuo000@163.com>
2020-09-23 13:58:58 +0800

    index link es

--- src/main/java/com/mycmv/index/controller/rest/elastic/ElasticIndexController.java
+++ src/main/java/com/mycmv/index/controller/rest/elastic/ElasticIndexController.java
@@ -1,13 +1,12 @@
 package com.mycmv.index.controller.rest.elastic;
 
-import com.alibaba.fastjson.JSON;
-import com.github.pagehelper.PageInfo;
 import com.mycmv.server.configuration.CurrentUser;
 import com.mycmv.server.configuration.UserLoginToken;
 import com.mycmv.server.constants.LogConstants;
 import com.mycmv.server.model.AbstractUser;
 import com.mycmv.server.model.ResponseObject;
-import com.mycmv.server.model.books.entry.BookInfo;
+import com.mycmv.server.model.books.elastic.BookInfoEs;
+import com.mycmv.server.service.elastic.ElasticService;
 import com.mycmv.server.utils.CommonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,22 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /***
  * Elastic
  * @author a
  */
 @RestController
-@RequestMapping("elastic/index")
+@RequestMapping("elastic")
 public class ElasticIndexController {
 
     private static final Logger logger = LoggerFactory.getLogger(LogConstants.STU_LOG);
 
-    @UserLoginToken
+    @Resource
+    private ElasticService elasticService;
+
     @ResponseBody
-    @GetMapping("create")
-    public ResponseObject create(@CurrentUser AbstractUser user, BookInfo bookItem, int pageIndex, int pageSize) {
-        String url = "book/pageList";
-        logger.info("用户 {} ，访问 {} , 参数：{}，{}，{}", user.getUserName(), url, JSON.toJSON(bookItem), pageIndex, pageSize);
+    @GetMapping("createBookInfoIndex")
+    public ResponseObject createBookInfoIndex() {
+        //logger.info("创建 BookInfo index,操作人:{}", user.getUserName());
+        elasticService.createIndex(BookInfoEs.class);
         ResponseObject responseObject = new ResponseObject();
         CommonUtils.executeSuccess(responseObject);
         return responseObject;
